2d46e278d779eeca9abb1bdb191e1ad09fb8d47e
SymNCgit config --global user.name SymNCgit config --global user.name SymNC <Sym.Cunningham@gmail.com>
2024-03-21 15:43:53 -0400

    added react context

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,20 @@ import { Routes, Route, Navigate } from 'react-router';
 import { Debug } from './Demo/genericpianomenus';
 import {PauseMenu} from './Demo/genericpianomenus/PauseMenu';
 import PlayPianoController from './pianoStateController/PlayPianoController';
+import { Settings } from './Demo/genericpianomenus/Settings';
+import React, { useContext } from 'react';
+
+export  function usePlayPianoController(): PlayPianoController {
+
+  const ctx = useContext(controllerContext);
+  return ctx;
+}
+
+export const controllerContext = React.createContext<PlayPianoController>(new PlayPianoController());
+
 
 function App() {
-  let controller = new PlayPianoController();
+  const controller = usePlayPianoController();
 
   return (
     <HashRouter>
@@ -20,6 +31,7 @@ function App() {
           <Route path='/Paused/*' element={<PauseMenu controller={controller}/>}></Route>
           <Route path='/SongSelect/*'   element={<SongSelect/>}></Route>
           <Route path='/debug/*'   element={<Debug controller = {controller}/>}></Route>
+          <Route path='/Settings/*' element={<Settings controller = {controller}/>}></Route>
 
         </Routes>
       </div>
