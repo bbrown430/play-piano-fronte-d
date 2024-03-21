01043ea75a61812c082b3ff07830be09b7412118
SymNCgit config --global user.name SymNCgit config --global user.name SymNC <Sym.Cunningham@gmail.com>
2024-03-21 15:42:41 -0400

    Listener pattern used for UI

--- src/pianoStateController/PlayPianoController.ts
+++ src/pianoStateController/PlayPianoController.ts
@@ -1,55 +1,94 @@
- type Mode = 'Learn' | 'Play' | 'Free' | 'Magic' | undefined;
- type Status = 'Paused' | 'inProgress' | 'Over' | undefined;
- type PianoSound = 'Grand' | 'Digital' | 'Organ' | undefined;
+import { PlayPianoEventHandler, PianoEventMap } from './PlayPianoEventHandler';
 
- interface  PianoSettings{
+ export type PianoMode = 'Learn' | 'Play' | 'Free' | 'Magic' | undefined;
+ export type PianoState = 'Menus' | 'Paused' | 'inProgress' | 'Over' | undefined;
+
+ export type PianoSound = 'Grand' | 'Digital' | 'Organ';
+
+
+
+
+
+ 
+
+ export interface  PianoSettings{
   pianoSound : PianoSound
-  //menu sound
+  // menu sound
   // background music?
-  //toggle : boolean
+  // toggle : boolean
   
 }
 
 
- type SongSettings = {
+ export type SongSettings = {
   song : string;
   tempo : number; //percentage 
   //hands : 'left' | 'right' | 'both';
 }
 
 export type State = {
-  mode : Mode;
-  status : Status;
+  mode : PianoMode;
+  status : PianoState;
   settings : PianoSettings;
   songSettings : SongSettings;
 
 }
 
-export default class PlayPianoController {
+export default class PlayPianoController{
+  private eventHandler : PlayPianoEventHandler;
   private _state : State;
+  constructor(){
+    this.eventHandler = new PlayPianoEventHandler()
+    this._state = {mode: undefined,
+                   settings:{pianoSound : 'Grand'},
+                   songSettings : {song : '???????', tempo : 100},
+                   status: undefined,
+                  }
+  
+  }
+
+  /**
+   * returns the current piano sound mode.
+   * @returns 
+   */
+  get pianoSound() : PianoSound {
+    return this._state.settings.pianoSound;
+  }
+  set pianoSound(pianoSound : PianoSound){
+    this._state.settings.pianoSound = pianoSound;
+  }
+  //gets the current 
+  changeSoundMode() {
+    switch(this._state.settings.pianoSound){
+      case 'Digital' :
+        this._state.settings.pianoSound='Organ'
+        break;
+      case 'Organ' :
+        this._state.settings.pianoSound='Grand'
+        break;
+      case 'Grand' :
+        this._state.settings.pianoSound='Digital'
+        break;
+
+      }
+    //emit that change
+  }
   unPause() {
     throw new Error('Method not implemented.');
   }
+
   restartSong() {
     throw new Error('Method not implemented.');
   }
 
-constructor(){
-  this._state = {mode: undefined,
-                 settings:{pianoSound : 'Grand'},
-                 songSettings : {song : '???????', tempo : 100},
-                 status: undefined,
-                }
-
-}
 
 
   //gets the current 
-  get mode() : Mode {
+  get mode() : PianoMode {
     return this._state.mode;
   }
 
-  set mode(mode : Mode) {
+  set mode(mode : PianoMode) {
     this._state.mode = mode;
   } 
 
@@ -65,11 +104,11 @@ constructor(){
     this._state.settings = settings;
   }
 
-  get status() : Status{
+  get status() : PianoState{
     return this._state.status;
   }
 
-  set status(status : Status) {
+  set status(status : PianoState) {
     this._state.status = status;
   }
 
@@ -78,12 +117,39 @@ constructor(){
   }
 
   set songSettings(songSettings : SongSettings) {
-    this._state.songSettings=songSettings;
+    this._state.songSettings = songSettings;
   }
+
+
+  public addListener<E extends keyof PianoEventMap>(
+    event: E,
+    listener: PianoEventMap[E],
+  ): this {
+    this.eventHandler.addListener(event,listener);
+    return this;
+  }
+
+  public removeListener<E extends keyof PianoEventMap>(
+    event: E,
+    listener: PianoEventMap[E],
+  ): this {
+    this.eventHandler.removeListener(event,listener);
+    return this;
+  }
+
+  public emit<E extends keyof PianoEventMap>(
+    event: E,
+    ...args: Parameters<PianoEventMap[E]>
+  ): boolean {
+    return this.eventHandler.emit(event,...args);
+  }
+  
+  
 
 
 
   // state to json or w/e to send to backend;
+
 
  
 }
---
+++ src/pianoStateController/PlayPianoEventHandler.ts
@@ -0,0 +1,65 @@
+import _ from 'lodash';
+import { EventMap } from 'typed-emitter';
+import { PianoMode, PianoState, PianoSound } from './PlayPianoController';
+
+
+export type PianoEventMap = EventMap & {
+  modeChange: (newMode: PianoMode) => void;
+  stateChange: (newState: PianoState) => void;
+  soundChange: (newSound: PianoSound) => void;
+};
+
+export class PlayPianoEventHandler {
+
+  private _listeners: Map<keyof PianoEventMap, PianoEventMap[keyof PianoEventMap][]> = new Map();
+
+
+
+  /**
+   * Add a listener for an event emitted by this InteractableAreaController
+   * @param event
+   * @param listener
+   * @returns
+   */
+  public addListener<E extends keyof PianoEventMap>(
+    event: E,
+    listener: PianoEventMap[E]
+  ): this {
+    const listeners = this._listeners.get(event) ?? [];
+    listeners.push(listener);
+    this._listeners.set(event, listeners);
+    return this;
+  }
+
+  /**
+   * Remove a listener for an event emitted by this InteractableAreaController
+   * @param event
+   * @param listener
+   * @returns
+   */
+  public removeListener<E extends keyof PianoEventMap>(
+    event: E,
+    listener: PianoEventMap[E]
+  ): this {
+    const listeners = this._listeners.get(event) ?? [];
+    _.remove(listeners, (l: PianoEventMap[E]) => l === listener);
+    this._listeners.set(event, listeners);
+    return this;
+  }
+
+  /**
+   * Emit an event to all listeners for that event
+   * @param event
+   * @param args
+   * @returns
+   */
+  public emit<E extends keyof PianoEventMap>(
+    event: E,
+    ...args: Parameters<PianoEventMap[E]>
+  ): boolean {
+    const listeners = this._listeners.get(event) ?? [];
+    listeners.forEach((listener) => { listener(...args); });
+    return true;
+  }
+
+}
