406231faee48be53023a08423dec869537ba151a
SymNCgit config --global user.name SymNCgit config --global user.name SymNC <Sym.Cunningham@gmail.com>
2024-04-07 18:04:59 -0400

    score goes to 100 on game end, effects loops separeted

--- src/Demo/PlayPage/ProgressHeader.tsx
+++ src/Demo/PlayPage/ProgressHeader.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import {usePlayPianoController } from "../../App";
 import { PPEvents } from "../../pianoStateController/PlayPianoEventHandler";
 import "./playpageformatting.css"
-import { useProgressFromServer, useScoreFromServer } from "../utils/APIHooks";
-import { command } from "yargs";
+import { useProgressFromServer, useScoreFromServer, useStatusFromServer } from "../utils/APIHooks";
 
 export function ProgressHeader(){
     const controller = usePlayPianoController();
     const [songTitle,setSongTitle] = useState(controller.songTitle);
+    const apiStatus = useStatusFromServer();
     const progress = useProgressFromServer();
     const score = useScoreFromServer()
     const [progressPercent,setProgressPercent] = useState("-1");
@@ -20,22 +20,28 @@ export function ProgressHeader(){
             setSongTitle(controller.currentSong.title)
         }
         
-        let percent = "-1"
-        if(controller.currentSong.end && progress <= controller.currentSong.end){
-          percent = (progress * 100 /controller.currentSong.end).toFixed();
-        }
-        setProgressPercent(percent)
-       
-        controller.currentSong.progress = progress;
-
         controller.addListener(PPEvents.SONG, songTitleListener);
         
         return () => {
             controller.removeListener(PPEvents.SONG, songTitleListener);
 
         };
-      }, [progress,score,controller, controller.pianoSound, controller.status, controller.songTitle]);
+      }, [controller]);
     
+      useEffect(()=>{
+        let percent = "-1"
+        if(controller.currentSong.end && progress <= controller.currentSong.end){
+          percent = (progress * 100 /controller.currentSong.end).toFixed();
+        }
+
+        controller.currentSong.progress = progress;
+
+        if(apiStatus === 'Over'){
+          percent = "100";
+        }
+
+        setProgressPercent(percent)
+      },[apiStatus, controller.currentSong, controller.currentSong.end, progress])
 
 
     return (
